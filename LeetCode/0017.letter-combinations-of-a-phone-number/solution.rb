716c637c15263bab97ecb1b87fed8b6884ad46b8
Jim Huang <hjijin.gz@gmail.com>
2022-05-31 14:26:57 +0800

    :sparkles: letter-combinations-of-a-phone-number

---
+++ LeetCode/0017.letter-combinations-of-a-phone-number/solution.rb
@@ -0,0 +1,33 @@
+# @param {String} digits
+# @return {String[]}
+def letter_combinations(digits)
+  alphanumerics = {
+    '2' => 'abc',
+    '3' => 'def',
+    '4' => 'ghi',
+    '5' => 'jkl',
+    '6' => 'mno',
+    '7' => 'pqrs',
+    '8' => 'tuv',
+    '9' => 'wxyz',
+  }
+
+  return [] if digits.length == 0
+
+  strs = []
+  digits.split('').each do |i|
+    strs = combination(strs, alphanumerics[digits[i]])
+  end
+
+  strs
+end
+
+def combination(strs, alphanumeric)
+  return alphanumeric.split('') if strs.empty?
+
+  strs = strs.map do |str|
+    alphanumeric.split('').map{|s| str + s}
+  end
+
+  strs
+end
